f61ad3663de9b630341495cb5211d319db9af854
Guilherme Ranoya <ranoya@gmail.com>
2024-04-22 12:47:27 -0300

    acelerando desenhos com P5

--- assets/meandros.js
+++ assets/meandros.js
@@ -7,6 +7,8 @@ export default ({
 
     let sketch = function (p) {
 
+        p.primeira = true;
+
 
         p.setup = function () {
 
@@ -29,6 +31,53 @@ export default ({
 
 
         p.draw = function () {
+
+            // Acelera o desenho
+            if (p.primeira) {
+                
+                for (let n = 0; n < 2000; n++) {
+
+                    p.push();
+                    p.translate(-document.getElementById(el).clientWidth / 1.7, 0);
+
+
+                    p.centroY = document.getElementById(el).clientHeight / 2;
+
+                    if (p.r1 < document.getElementById(el).clientWidth * 2) {
+
+
+                        p.line(p.cos(p.a) * p.r1, (p.sin(p.a) * p.r1) + p.centroY, p.cos(p.a) * p.r2, (p.sin(p.a) * p.r2) + p.centroY);
+                        p.count++;
+                        p.a = p.a + (p.step * p.direction);
+
+                        if (p.a > Math.PI / 9 || p.a < -Math.PI / 9 || p.count > (80 + Math.random() * 220)) {
+
+                            p.count = 0;
+                            p.step = 0.001 + Math.random() / 290;
+                            p.r1 = p.r2 + 2;
+                            p.r2 = p.r1 + 10 + (Math.random() * 110);
+
+                            if (p.a > Math.PI / 9 || p.a < -Math.PI / 9 || Math.random() > 0.5) {
+                                p.direction *= -1;
+                                p.a = p.a + (p.step * p.direction);
+                                //console.log(p.a);
+
+                            }
+
+                        }
+
+
+
+
+                    }
+
+                    p.pop();
+
+
+                }
+
+                p.primeira = false;
+            }
 
             p.push();
             p.translate(-document.getElementById(el).clientWidth / 1.7, 0);
